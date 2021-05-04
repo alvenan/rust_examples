cab9f9f89a90779be90f359c37415482e8c2f74c
Alison Venancio <alisonv@eldorado.org.br>
2021-05-04 19:42:39 -0400

    Removing and ignoring Lock files

---
+++ hello_vs/target/debug/deps/hello_world-80b0ef66eda23640.d
@@ -0,0 +1,5 @@
+/home/alisonv/Documents/code/rust_examples/hello_vs/target/debug/deps/hello_world-80b0ef66eda23640: src/main.rs
+
+/home/alisonv/Documents/code/rust_examples/hello_vs/target/debug/deps/hello_world-80b0ef66eda23640.d: src/main.rs
+
+src/main.rs:
---
+++ hello_vs/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/alisonv/Documents/code/rust_examples/hello_vs/target/debug/hello_world: /home/alisonv/Documents/code/rust_examples/hello_vs/src/main.rs
