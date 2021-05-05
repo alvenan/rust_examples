1c82a0e617ed82baee79d9c3eacb46fbf78adf24
Álison Venâncio <alison.venancio@gmail.com>
2021-05-05 13:33:17 -0400

    Delete loops/target directory

--- loops/target/rls/debug/deps/loops-99c40d3d6164a5f4.d
+++
@@ -1,5 +0,0 @@
-/home/alisonv/Documents/code/rust_examples/loops/target/rls/debug/deps/loops-99c40d3d6164a5f4.rmeta: src/main.rs
-
-/home/alisonv/Documents/code/rust_examples/loops/target/rls/debug/deps/loops-99c40d3d6164a5f4.d: src/main.rs
-
-src/main.rs:
--- loops/target/rls/debug/deps/loops-c2a07360bbc48ffe.d
+++
@@ -1,5 +0,0 @@
-/home/alisonv/Documents/code/rust_examples/loops/target/rls/debug/deps/loops-c2a07360bbc48ffe.rmeta: src/main.rs
-
-/home/alisonv/Documents/code/rust_examples/loops/target/rls/debug/deps/loops-c2a07360bbc48ffe.d: src/main.rs
-
-src/main.rs:
