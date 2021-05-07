8b508e973e0fcf8da7fea1a87dc705b3e1c16da2
Alison Venancio <alison.venancio@gmail.com>
2021-05-07 19:28:17 -0400

    remove trash

--- stack_heap/target/debug/deps/stack_heap-14918f8b5cc7d0cf.d
+++
@@ -1,6 +0,0 @@
-/home/alisonv/Documents/code/rust_examples/stack_heap/target/debug/deps/stack_heap-14918f8b5cc7d0cf: src/main.rs src/stack_heap.rs
-
-/home/alisonv/Documents/code/rust_examples/stack_heap/target/debug/deps/stack_heap-14918f8b5cc7d0cf.d: src/main.rs src/stack_heap.rs
-
-src/main.rs:
-src/stack_heap.rs:
--- stack_heap/target/rls/debug/deps/stack_heap-d7ffa90b8f1d4b9e.d
+++
@@ -1,6 +0,0 @@
-/home/alisonv/Documents/code/rust_examples/stack_heap/target/rls/debug/deps/stack_heap-d7ffa90b8f1d4b9e.rmeta: src/main.rs src/stack_heap.rs
-
-/home/alisonv/Documents/code/rust_examples/stack_heap/target/rls/debug/deps/stack_heap-d7ffa90b8f1d4b9e.d: src/main.rs src/stack_heap.rs
-
-src/main.rs:
-src/stack_heap.rs:
